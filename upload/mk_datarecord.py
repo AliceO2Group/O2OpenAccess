bc6c4b9cb2613e8b72d5679b57258f788d5ffa12
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2025-02-11 16:13:51 +0200

    use  in mk_datarecord.py

--- upload/mk_datarecord.py
+++ upload/mk_datarecord.py
@@ -8,13 +8,13 @@ import zlib
 import json
 import recordclass
 from datetime import datetime
+from rich.pretty import pprint
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from opendata_record import *
 
 input_file_index = 'LHC10h_000139510_PbPb_2.76TeV_205656_file_index.json'
-
 
 fidx_list = {}
 try:
@@ -24,10 +24,7 @@ except Exception as e:
     print(f'Could not load {input_file_index}\n{e}')
     sys.exit(1)
 
-from rich.pretty import pprint
-
-file_list_rec = fileindexRec()
-
+##################################################
 sum_files = int(0)
 sum_size = int(0)
 file_list = []
@@ -42,10 +39,6 @@ for idx,f in enumerate(fidx_list):
     sum_size += int(f_rec.size)
     file_list.append(recordclass.asdict(f_rec))
 
-
-# print(f'Nr files: {sum_files}')
-# print(f'Total size: {sum_size}')
-
 ##################################################
 file_indices = fileindexRec()
 file_indices.files = file_list
@@ -58,6 +51,8 @@ file_indices_dict = recordclass.asdict(file_indices)
 file_metadata = metadataRec()
 file_metadata._file_indices = [ file_indices_dict ]
 file_metadata_dict = recordclass.asdict(file_metadata)
+file_metadata_dict['$schema'] = file_metadata_dict['schema']
+file_metadata_dict.pop('schema')
 
 ##################################################
 run_record = runRec()
@@ -76,6 +71,4 @@ record_dict = recordclass.asdict(run_record)
 record_file_name = input_file_index.replace('_file_index.json','') + '_record.json'
 record_out = json.dumps(record_dict, indent = 4) + '\n'
 with open(record_file_name, 'wb') as f: f.write(record_out.encode("utf-8"))
-
-
 
